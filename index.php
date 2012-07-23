99ac89b1b0bd7598f3ccc347071fb9953ed93d1b
i-masik <zhernosek.maxim@gmail.com>
2012-07-23 12:30:48 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Снежки</title>
+        <link rel="stylesheet" href="reset.css" type="text/css"/>
+        <link rel="stylesheet" href="css/main_style.css" type="text/css"/>
+    </head>
+    <body>
+       
+            <div id="header">
+                <div id="header_background"></div>
+            </div>
+             <div id="main">
+            <div id="content">Контент</div>
+            <div id="footer">Футер</div>
+        </div>
+    </body>
+</html>
